825150ef119ac7e1192e011af55ef00e2c01ee27
fulviocanducci <fulviocanducci@hotmail.com>
2018-03-28 12:01:53 -0300

    Update 2 Beta 3

--- Canducci.Zip.WebSiteApiAngular/Controllers/ZipCodeAddressController.cs
+++ Canducci.Zip.WebSiteApiAngular/Controllers/ZipCodeAddressController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Canducci.Zip.WebSiteApiAngular.Models;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Canducci.Zip.WebSiteApiAngular.Controllers
 {
@@ -44,19 +45,34 @@ namespace Canducci.Zip.WebSiteApiAngular.Controllers
         [ResponseCache(Duration = 3600)]
         public async Task<IActionResult> AddressCodePost([FromBody] Dictionary<string, string> values)
         {
-            //var zip = values["zip"];
-            //if (!String.IsNullOrEmpty(zip))
-            //{
-            //    if (ZipCode.TryParse(zip, out ZipCode zipCode))
-            //    {
-            //        var result = await ZipCodeLoad.FindAsync(zipCode);
-            //        if (result)
-            //        {
-            //            return Json(result.Value);
-            //        }
-            //    }
-            //}
-            return Json(new { });
+            var uf = values["uf"];
+            var city = values["city"];
+            var address = values["address"];
+            if (AddressCode.TryParse(Enum.Parse<ZipCodeUf>(uf), city, address, out AddressCode addressCode))
+            {
+                AddressCodeResult addressCodeResult = await AddressCodeLoad.FindAsync(addressCode);
+                if (addressCodeResult)
+                {
+                    return Json(addressCodeResult.Value);
+                }
+            }            
+            return Json(await Task.FromResult<object>(null));
+        }
+
+
+        [Route("uftolist")]
+        [HttpPost()]
+        [ResponseCache(Duration = 3600)]
+        public IActionResult UfToList()
+        {
+            return Json(AddressCodeLoad
+                .UFToList()
+                .Select(x => new
+                {
+                    Name = x.Key,
+                    x.Value
+                })
+            .ToList());
         }
     }
 }
--- Canducci.Zip.WebSiteApiAngular/wwwroot/js/app.js
+++ Canducci.Zip.WebSiteApiAngular/wwwroot/js/app.js
@@ -2,6 +2,25 @@
 var focus = function (element) {
     document.getElementById(element).focus();
 }
+app.run(function ($rootScope, $http) {
+    $rootScope.ufs = [];
+    $rootScope.ufLoad = function () {
+        $http({
+            url: '/uftolist',
+            method: "POST",
+            data: { },
+
+        }).then(function (response) {
+            $rootScope.ufs = response.data;  
+            console.log($rootScope.ufs);
+        }, function (response) {
+            console.log('Error');
+            console.log(response);
+        });
+    }
+    $rootScope.ufLoad();
+});
+
 app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', {
@@ -48,10 +67,34 @@ app.controller("zipCodeCtrl", ["$scope", "$location", "$http", function ($scope,
     $scope.clear = function () {
         $scope.model = $scope.modelInit();
         $scope.zip = '';
+        focus("zip")
     }
     $scope.clear();
 }]);
 
-app.controller("addressCtrl", ["$scope", "$location", "$http", function ($scope, $location, $http) {
-    
+app.controller("addressCtrl", ["$scope", "$location", "$http", "$rootScope", function ($scope, $location, $http, $rootScope) {
+    $scope.model = {};
+    $scope.items = [];
+    $scope.modelInit = function () {
+        $scope.model = { 'uf': { "name": "SP", "value": "SP" }, 'city': '', 'address': '' };
+    }
+    $scope.modelInit();
+    $scope.clear = function () {
+        $scope.modelInit();
+        focus("uf");
+    }
+    $scope.find = function () {
+        $http({
+            url: '/addresscode',
+            method: "POST",
+            data: { 'uf': $scope.model.uf.value, 'city': $scope.model.city, 'address': $scope.model.address },
+
+        }).then(function (response) {
+            $scope.items = response.data;
+            console.log($scope.items);
+        }, function (response) {
+            console.log('Error');
+            console.log(response);
+        });
+    };
 }]);
