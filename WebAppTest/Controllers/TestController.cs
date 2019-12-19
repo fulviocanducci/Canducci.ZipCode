3ad6c919d1fa04c8018110b670680745dbc44019
Fulvio <fulviocanducci@hotmail.com>
2019-12-19 14:40:26 -0300

    Save Project Web

---
+++ WebAppTest/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Canducci.Zip.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTest.Controllers
+{
+   public class TestController : Controller
+   {
+      public IZipCodeLoad ZipCodeLoad { get; }
+      public IAddressCodeLoad AddressCodeLoad { get; }
+
+      public TestController(IZipCodeLoad zipCodeLoad, IAddressCodeLoad addressCodeLoad)
+      {
+         ZipCodeLoad = zipCodeLoad;
+         AddressCodeLoad = addressCodeLoad;
+      }
+
+      public IActionResult Index()
+      {
+         return View();
+      }
+   }
+}
