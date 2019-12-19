b87eb849246b048cc2ec7aebab15b4e4f7240751
Fulvio <fulviocanducci@hotmail.com>
2019-12-19 14:26:01 -0300

    Extensions MVC Project

---
+++ Canducci.Zip.Mvc/CanducciZipMvcExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Canducci.Zip.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+namespace Canducci.Zip.Mvc
+{
+   public static class CanducciZipMvcExtensions
+   {
+      public static IServiceCollection AddZipCode(this IServiceCollection service)
+      {
+         return service.AddScoped<IZipCodeLoad, ZipCodeLoad>();
+      }
+
+      public static IServiceCollection AddAddressCode(this IServiceCollection service)
+      {
+         return service.AddScoped<IAddressCodeLoad, AddressCodeLoad>();
+      }
+
+      public static IServiceCollection AddZipCodeAndAddressCode(this IServiceCollection service)
+      {
+         return service
+            .AddZipCode()
+            .AddAddressCode();
+      }
+   }
+}
